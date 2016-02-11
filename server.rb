04ad268c6c45a1d70412e456f66c5eff945b7567
Gerard T McGuire <gtm248@email.vccs.edu>
2016-02-10 20:32:05 -0500

    server knows if a get or post request was sent

--- server.rb
+++ server.rb
@@ -1,15 +1,13 @@
-require 'socket'               # Get sockets from stdlib
+require 'socket'              # Get sockets from stdlib
+require 'json'
 
-@server = TCPServer.open(2000)  # Socket to listen on port 2000
 
 def handle_request(request)
  	base = request.split
  	@method = base[0]
  	@path = base[1]
  	@vers = base[2]
- 	
-
- end
+end
 
 def get_req
 	if File.exist?(@path) == true
@@ -20,22 +18,34 @@ def get_req
   		File.open(@path).each do |line|
   			@client.puts "#{line}"
   		end
-  	
   	else
   		@client.puts "404 Not Found!"
   	end
 end
 
+def post_req
+	@client.puts "#{@method} #{@path} #{@vers}"
 
 
+end
+
+def handler
+	if @method == "GET"
+		get_req
+	elsif @method == "POST"
+		post_req
+	end
+end
+
+
+@server = TCPServer.open(2000)  # Socket to listen on port 2000
 
 loop do                        # Servers run forever
   @client = @server.accept       # Wait for a client to connect
   @request = @client.gets
  
   handle_request(@request)
-  get_req
-
+  handler
   
   @client.close                 # Disconnect from the client
 
--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -1,4 +1,5 @@
 require "socket"
+require "json"
 
 @host = "localhost"				# The web server
 @port = 2000									# Default HTTP port
@@ -18,17 +19,20 @@ puts @response
 #print body
 end								# And display it.
 
-
-
-
-
-
 input = " "
 while input != "exit"
 	print "What would you like to do: "
-	input = gets.chomp.downcase
+	input = gets.chomp
 	if input == "get"
 		make_req("GET #{@path} HTTP/1.0\r\n\r\n")
+	elsif input == "post"
+		print "Whats the name of the viking?: "
+		@name = gets.chomp
+		print "What is the vikings email adress?: "
+		@email = gets.chomp
+		hash_in_hash = {:viking => {:name=> @name, :email=> @email} }
+		@data = hash_in_hash.to_json
+		make_req("POST #{@data} HTTP/1.0\r\n\r\n")
 	end
 
 end
